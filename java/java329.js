53545c6a842b8ae70ea99135792c3ed1b9f142d9
Piyushkumarmandal <Piyushmandal408@gmail.com>
2022-11-27 20:53:15 +0530

    initial commit
    asdfghjkl;

---
+++ java/java329.js
@@ -0,0 +1,53 @@
+alert ("ARE U INTERSECTED IN GYM")
+let ok=prompt("what is ur name")
+let c=document.getElementsByClassName("BEST")[0]
+c.style.color ="whitesmoke";
+let n=document.querySelectorAll(".GYME")
+document.querySelectorAll(".GYME")[0].onmouseleave=nn;
+function nn(){
+    document.querySelectorAll(".GYME")[0].style.background="rgb(37, 27, 55)"
+    document.querySelectorAll(".GYME")[0].style.color="rgb(255, 202, 202)"
+}
+document.querySelectorAll(".CONDITION")[0].style.color="black";
+document.getElementById("navbar").onmouseenter=abc;
+function abc(){document.getElementById("navbar").style.background="white";
+}
+document.getElementById("K").onmouseleave =YELOW;
+    function YELOW() {document.getElementById("K").style.background="rgb(255, 202, 202)";
+    
+}
+let h=document.getElementById("pppp")
+
+h=document.getElementById("pppp").onmouseenter=l;
+function l() {document.getElementById("pppp").textContent="HELLO";
+
+    
+}
+ document.getElementById("TRA").onmouseleave=mouse;
+ function mouse(){document.getElementById("TRA").style.color="rgb(255, 202, 202)"
+ document.getElementById("TRA").style.background="rgb(37, 27, 55)"
+
+
+ }
+ document.getElementById("PLAN").onmouseleave=pl;
+ function pl(){document.getElementById("PLAN").style.color="rgb(255, 202, 202)"
+document.getElementById("PLAN").style.background="rgb(37, 27, 55)"}
+function functionk(element){
+    element.style.background="pink" ;
+}
+function nocolor(element){
+   element.style.background="";
+}
+
+document.getElementById("mk").onmouseenter=pk;
+function pk() {document.getElementById("mk").style.background="rgb(55, 41, 72"}
+    
+
+document.getElementById("mk").onmouseleave=un;
+function un(){document.getElementById("mk").style.background=""}
+document.getElementById("navbar").onmouseleave=om;
+function om() {document.getElementById("navbar").style.background=""
+    
+}
+document.getElementById("ok").classList.add("newone")
+
